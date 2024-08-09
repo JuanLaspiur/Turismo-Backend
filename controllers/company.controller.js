3f4933847133b681a600d9d0d8b522944159c059
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-08-09 16:09:35 -0300

    Gestión de la img de Company

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -1,11 +1,29 @@
 const { response } = require("express");
 const Company = require('../models/Company');
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
 
-
-
-
-
-
+const saveImage = async (base64Data, companyID) => {
+    const matches = base64Data.match(/^data:image\/([a-zA-Z]*);base64,/);
+    if (!matches || matches.length !== 2) {
+      return null;
+    }  
+    const ext = matches[1];
+    const imageBuffer = Buffer.from(base64Data.replace(/^data:image\/[a-zA-Z]*;base64,/, ''), 'base64');
+  
+    const uploadDir = path.join('./', 'assets', 'companies','img');
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+  
+    const fileName = `${companyID}.webp`;
+    const filePath = path.join(uploadDir, fileName);
+  
+    await sharp(imageBuffer)
+      .toFormat('webp')
+      .toFile(filePath);
+  };
 
 const getCompanies = async (req, res = response) => {
     try {
@@ -51,6 +69,13 @@ const createCompany = async (req, res = response) => {
     try {
         const company = new Company({ name, description, img, coverImg });
         await company.save();
+
+        if (img && typeof img === 'string') {
+            await saveImage(img, company._id);
+            company.img = `${company._id}.webp`;
+            await company.save();
+          }
+
         res.status(201).json({
             success: true,
             data: company
@@ -68,12 +93,19 @@ const updateCompany = async (req, res = response) => {
     const { id } = req.params;
     try {
         const company = await Company.findByIdAndUpdate(id, req.body, { new: true });
-        if (!company) {
+            if (!company) {
             return res.status(404).json({
                 success: false,
                 msg: 'Compañía no encontrada'
             });
         }
+
+        if (img && typeof img === 'string') {
+            await saveImage(img, company._id);
+            company.img = `${company._id}.webp`;
+            await company.save();
+          }
+
         res.json({
             success: true,
             data: company
