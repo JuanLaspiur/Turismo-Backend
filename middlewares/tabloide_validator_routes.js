4f43fb798370ba34e8b420d4fb40960270b25d53
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-08-09 14:02:29 -0300

    Guardado

--- middlewares/tabloide_validator_routes.js
+++ middlewares/tabloide_validator_routes.js
@@ -2,10 +2,7 @@ const { check } = require("express-validator");
 
 const tabloideValidations = {
   createTabloide: [
-    check(
-      "nro_posicion",
-      "nro_posicion es requerido y debe ser un número entero."
-    ).isInt(),
+    check("nro_posicion","nro_posicion es requerido y debe ser un número entero.").isInt(),
     check("status", "status es requerido y debe ser un número entero.").isInt(),
     check("redirect", "redirect es requerido y debe ser una cadena."),
     check("img", "img debe ser un array de cadenas.").isString(),
